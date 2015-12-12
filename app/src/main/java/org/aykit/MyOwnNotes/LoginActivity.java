7c747c4f64b8d20a9eae21116d0f6ef56b7515a1
mklepp <me@mklepp.com>
2015-12-12 16:20:44 +0100

    Use nice custom list for account selection

--- app/src/main/java/org/aykit/MyOwnNotes/LoginActivity.java
+++ app/src/main/java/org/aykit/MyOwnNotes/LoginActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.CoordinatorLayout;
@@ -14,8 +13,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import com.afollestad.materialdialogs.DialogAction;
@@ -27,19 +28,22 @@ import com.owncloud.android.lib.common.operations.RemoteOperationResult;
 import com.owncloud.android.lib.resources.files.CreateRemoteFolderOperation;
 import com.owncloud.android.lib.resources.files.FileUtils;
 import com.owncloud.android.lib.resources.files.ReadRemoteFolderOperation;
-import com.owncloud.android.lib.resources.files.RemoteFile;
 
 import org.aykit.MyOwnNotes.helpers.Settings;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class LoginActivity extends AppCompatActivity {
-
-    private static final int PICK_ACCOUNT_REQUEST = 12412;
+public class LoginActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     @Bind(R.id.coordinatorlayout)
     CoordinatorLayout coordinatorLayout;
+
+    @Bind(android.R.id.list)
+    ListView listView;
 
     @Bind(android.R.id.empty)
     Button emptyButton;
@@ -52,22 +56,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-        showAccounts();
+
+        listView.setEmptyView(emptyButton);
+        listView.setOnItemClickListener(this);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            switch (requestCode) {
-                case PICK_ACCOUNT_REQUEST: {
-                    String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-                    PreferenceManager.getDefaultSharedPreferences(LoginActivity.this).edit().putString(Settings.PREF_ACCOUNT_NAME, accountName).apply();
-                    PreferenceManager.getDefaultSharedPreferences(LoginActivity.this).edit().remove(Settings.PREF_ACCOUNT_PASSWORD).apply();
-                    checkStoredAccount();
-                    break;
-                }
-            }
-        }
+    protected void onResume() {
+        super.onResume();
+
+        showAccounts();
     }
 
     private void queryPassword(final Account account) {
@@ -142,16 +140,18 @@ public class LoginActivity extends AppCompatActivity {
 
         Account[] accounts = AccountManager.get(this).getAccountsByType(getString(R.string.account_type));
 
+        List<String> accountNames = new ArrayList<>();
+
+        for (Account account: accounts){
+            accountNames.add(account.name);
+        }
+
         if (accounts.length == 0) {
             emptyButton.animate().alpha(1);
+            listView.animate().alpha(0);
         } else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                startActivityForResult(AccountManager.newChooseAccountIntent(null, null, new String[]{getString(R.string.account_type)}, null,
-                        null, null, null), PICK_ACCOUNT_REQUEST);
-            } else {
-                startActivityForResult(AccountManager.newChooseAccountIntent(null, null, new String[]{getString(R.string.account_type)}, true, null,
-                        null, null, null), PICK_ACCOUNT_REQUEST);
-            }
+            listView.animate().alpha(1);
+            listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, accountNames));
         }
     }
 
@@ -163,11 +163,20 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String accountName = listView.getAdapter().getItem(position).toString();
+        PreferenceManager.getDefaultSharedPreferences(LoginActivity.this).edit().putString(Settings.PREF_ACCOUNT_NAME, accountName).apply();
+        PreferenceManager.getDefaultSharedPreferences(LoginActivity.this).edit().remove(Settings.PREF_ACCOUNT_PASSWORD).apply();
+        checkStoredAccount();
+    }
+
     private class CheckAccountAsyncTask extends AsyncTask<Account, Void, Account> {
         protected void onPreExecute() {
             // Runs on the UI thread before doInBackground
             // Good for toggling visibility of a progress indicator
             progressBar.animate().alpha(1);
+            listView.animate().alpha(0);
         }
 
         @Override
