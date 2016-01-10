a8ffb908642a84bf4ec49c29c70f5b65c57820f4
mklepp <me@mklepp.com>
2016-01-10 15:26:17 +0100

    Optimise imports

--- app/src/main/java/org/aykit/MyOwnNotes/activities/NoteListActivity.java
+++ app/src/main/java/org/aykit/MyOwnNotes/activities/NoteListActivity.java
@@ -15,11 +15,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.DialogAction;
